9622acd6f0d0e334744afe3fc2c7a5be2bac1711
nifie <kuper116rus.2005@gmail.com>
2023-02-27 05:57:41 +0300

    qwe

---
+++ laba8/MyFig.cs
@@ -0,0 +1,17 @@
+﻿namespace laba8
+{
+    internal class MyFig : Figure
+    {
+        public MyFig(double x, double y, double wid, double hei):base(x,y,wid,hei) { }
+
+        public override void Draw()
+        {
+            Graphics g = Graphics.FromImage(Init.bitmap);
+            g.DrawRectangle(Init.pen, (int)X, (int)(Y +(Height/2)), (int)Width, (int)Height /2);
+            g.DrawEllipse(Init.pen, (int)X+2, (int)(Y + (Height / 3)), (int)Width / 2, (int)Height / 4);
+            g.DrawPolygon(Init.pen, );
+
+            Init.pictureBox.Image = Init.bitmap;
+        }
+    }
+}
